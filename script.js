bb236880fc865987087dc099df0858c4c0f7aa85
1canas <75550719+1canas@users.noreply.github.com>
2020-12-12 00:40:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+const keys = document.getElementsByClassName('key');
+
+function playNote(event) {
+    let audioKeyCode = getKeyCode(event);
+    const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+
+    if(key == false) {
+        return;
+    }
+
+    addPlayingClass(key);
+    playAudio(audioKeyCode);
+}
+
+function addPlayingClass(key) {
+    key.classList.add('playing')
+}
+
+function removePlayingClass(event) {
+    event.target.classList.remove('playing')
+}
+
+function getKeyCode(event) {
+    let keyCode;
+
+    const keyboardPressed = event.type === 'keydown'
+    if(keyboardPressed) {
+        keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+    }
+
+    return keyCode
+}
+
+function playAudio(audioKeyCode) {
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+    audio.currentTime = 0;
+    audio.play()
+}
+
+function eventRegister() {
+    //mouse
+    console.log(keys[1])
+
+    for(key of keys) {
+        key.addEventListener('click', playNote)
+        key.addEventListener('transitionend', removePlayingClass)
+    }
+
+    //keyboard
+    window.addEventListener('keydown', playNote)
+}
+
+window.addEventListener("load", eventRegister)
